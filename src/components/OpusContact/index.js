760271403b637ba64721f0b44a9a9016f2c4f4fe
Gabriela Dias de Souza <macbook-gabs@MacBook-Pro-de-Gabriela-2.local>
2019-11-14 20:09:17 -0300

    Transform all components to functional ones

--- src/components/OpusContact/index.js
+++ src/components/OpusContact/index.js
@@ -1,36 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import "./OpusContact.css";
 import { Link } from "react-router-dom";
-import Service from "../../service";
 
-class OpusContact extends Component {
-  constructor(props) {
-    super(props);
-    this.service = new Service();
-  }
-
-  render() {
-    return (
-      <section className="opus-contact flex-direction-row width-12">
-        <div className="flex-direction-column">
-          <div className="flex-direction-row">
-            <img className="user" src="/user.svg" alt="User Icon" />
-            <p className="margin-10">{this.props.name}</p>
-            <p>({this.props.ddd})</p>
-            <p className="margin-10">{this.props.telephone}</p>
-          </div>
+const OpusContact = props => {
+  return (
+    <section className="opus-contact flex-direction-row width-12">
+      <div className="flex-direction-column">
+        <div className="flex-direction-row">
+          <img className="user" src="/user.svg" alt="User Icon" />
+          <p className="margin-10">{this.props.name}</p>
+          <p>({this.props.ddd})</p>
+          <p className="margin-10">{this.props.telephone}</p>
         </div>
-        <div>
-          <Link to={"/edit/" + this.props.id}>
-            <img id="edit-button" src="/edit-24px.svg" alt="Edit Icon" />
-          </Link>
-          <button className="no-border" onClick={this.props.onClick}>
-            <img src="/delete-24px.svg" alt="Delete Icon" />
-          </button>
-        </div>
-      </section>
-    );
-  }
-}
+      </div>
+      <div>
+        <Link to={"/edit/" + this.props.id}>
+          <img id="edit-button" src="/edit-24px.svg" alt="Edit Icon" />
+        </Link>
+        <button className="no-border" onClick={this.props.onClick}>
+          <img src="/delete-24px.svg" alt="Delete Icon" />
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default OpusContact;
--- src/pages/EditPage/index.js
+++ src/pages/EditPage/index.js
@@ -1,36 +1,84 @@
-import React, { Component } from "react";
-import OpusContactDetails from "../../components/OpusContactDetails";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Service from "../../service";
+import OpusTitle from "../../components/OpusTitle";
+import OpusSafeArea from "../../components/OpusSafeArea";
 
-class Edit extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      ddd: 0,
-      telephone: 0
-    };
-    this.service = new Service();
-  }
+const OpusEditContact = props => {
+  const [values, setValues] = useState({ name: "", ddd: "", telephone: "" });
+  const service = new Service();
 
-  componentDidMount() {
-    this.service.getOneContact(this.props.match.params.id).then(response => {
-      this.setState({
-        name: response[0].name,
-        ddd: response[0].ddd,
-        telephone: response[0].telephone
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const { name, ddd, telephone } = values;
+    service
+      .addContact(name, ddd, telephone)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(e => console.log(e));
+  };
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  useEffect(() => {
+    service.getOneContact(props.match.params.id).then(response => {
+      console.log(response);
+      setValues({
+        name: response.name,
+        ddd: response.ddd,
+        telephone: response.telephone
       });
     });
-  }
+  });
 
-  render() {
-    return (
-      <OpusContactDetails
-        name={this.state.name}
-        ddd={this.state.ddd}
-        telephone={this.state.telephone}
-      />
-    );
-  }
-}
-export default Edit;
+  return (
+    <OpusSafeArea>
+      <section className="opus-contact-details flex-direction-column align-text-center width-12">
+        <div className="flex-direction-row width-12">
+          <Link to={"/"}>
+            <img id="edit-button" src="/arrow_back-24px.svg" alt="Edit Icon" />
+          </Link>
+          <OpusTitle>Editar Contato</OpusTitle>
+
+          <Link to={"/"}>
+            <img id="edit-button" src="/close-24px.svg" alt="Edit Icon" />
+          </Link>
+        </div>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="opus-input width-12"
+            name="name"
+            value={values.name}
+            onChange={handleInputChange}
+            placeholder="Nome Completo"
+          ></input>
+          <div className="flex-direction-row width-12">
+            <input
+              className="opus-input width-3"
+              name="ddd"
+              value={values.ddd}
+              onChange={handleInputChange}
+              placeholder="DDD"
+            ></input>
+            <input
+              className="opus-input width-9"
+              name="telephone"
+              value={values.telephone}
+              onChange={handleInputChange}
+              placeholder="Celular"
+            ></input>
+          </div>
+          <button type="submit" className="long button">
+            OK
+          </button>
+        </form>
+      </section>
+    </OpusSafeArea>
+  );
+};
+
+export default OpusEditContact;
